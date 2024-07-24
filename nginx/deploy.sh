d3f7eabb6965ae69500287a9cde0cf32b4682817
Dor Amar <doramar5555@gmail.com>
2024-07-24 19:50:06 +0700

    nginx cicd

---
+++ nginx/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker stop nginx
+docker rm nginx
+
+docker run -d \
+  --name nginx \
+  --network netflix \
+  -p 80:80 \
+  -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
+  nginx:latest
