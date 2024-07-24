5749fcc076a5c49a8a1e57e67f3af3d7bc911184
Dor Amar <doramar5555@gmail.com>
2024-07-24 19:54:32 +0700

    Path fix on nginx cicd

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -7,5 +7,5 @@ docker run -d \
   --name nginx \
   --network netflix \
   -p 80:80 \
-  -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v $(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
   nginx:latest
