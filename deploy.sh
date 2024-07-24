9c420af842f9002172f61cbb4fb37bfa2f74a216
Dor Amar <doramar5555@gmail.com>
2024-07-24 19:11:04 +0700

    fix key reffrence

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # Variables
-CONTAINER_NAME="netflix-movie-catalog"
-IMAGE_NAME="doramar97/netflix-movie-catalog:latest"
+CONTAINER_NAME="netflix-frontend"
+IMAGE_NAME="doramar97/netflix-frontend:latest"
 
-# Stop and remove the existing container if it exists
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# Pull the latest Docker image
 docker pull $IMAGE_NAME
 
 # Run the new container
